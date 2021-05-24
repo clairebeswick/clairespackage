db1dca6723bed4eb677ef7791af1c7f78976f48d
Claire Beswick <claire.beswick.h@gmail.com>
2021-05-24 15:04:27 +0200

    second push

--- clairespackage/lib.py
+++ clairespackage/lib.py
@@ -2,7 +2,8 @@ import webbrowser
 
 
 def try_me():
-    webbrowser.open(f'https://www.youtube.com/watch?v=P13ApHECCyY', new=1)
+    webbrowser.open(f'https://www.youtube.com/watch?v=dQw4w9WgXcQ', new=1)
+
 
 if __name__ == "__main__":
     try_me()
